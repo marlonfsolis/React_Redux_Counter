3e7cc64cfa5c1110f62dfc7a309f523645ba0182
Marlon Fernandez <mfernandez@coadvantage.com>
2021-12-21 09:46:45 -0500

    Adding a not found page

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import App from './App';
 import Home from './views/Home/Home';
 import About from './views/About/About';
 import CounterRedux from './views/CounterRedux/CounterRedux';
+import NoFound from './views/NotFound/NotFound';
 
 const Router = () => {
 	return (
@@ -16,6 +17,7 @@ const Router = () => {
 					<Route path='about' element={<About />}></Route>
 					<Route path='counter/redux' element={<CounterRedux />}></Route>
 				</Route>
+				<Route path='*' element={<NoFound />}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/views/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <main className='container'>
+            <h1>Page not found</h1>
+            <p>There is nothing here!</p>
+        </main>
+    );
+};
+
+export default NotFound;
